d74e7215c2f4cce6cc5831dd14ba35f829547cda
huanweirong <1091189763@qq.com>
2024-01-23 00:25:38 +0800

    fix: build on win

--- xmake.lua
+++ xmake.lua
@@ -1,9 +1,15 @@
 add_rules("mode.debug", "mode.release")
 set_languages("c17", "c++17")
 
-add_requires("imgui 1.90", {configs = {glfw_opengl3 = true}})
 add_requires("glfw 3.3")
-add_requires("spdlog 1.13")
+
+if is_plat("windows") then
+    add_requires("imgui v1.90.1", {configs = {glfw_opengl3 = true}})
+    add_requires("spdlog v1.13.0")
+else
+    add_requires("imgui 1.90", {configs = {glfw_opengl3 = true}})
+    add_requires("spdlog 1.13")
+end
 
 target("pilot")
     set_kind("binary")
